6c19047e6b6304856334571a1fa865f52de46a08
PSG-00 <cdjsdj1902@gmail.com>
2026-09-18 18:19:01 +0900

    Initial commit

---
+++ src/main/java/com/formlimpic/FormlimpicApplication.java
@@ -0,0 +1,13 @@
+package com.formlimpic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormlimpicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormlimpicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/formlimpic/FormlimpicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formlimpic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormlimpicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
